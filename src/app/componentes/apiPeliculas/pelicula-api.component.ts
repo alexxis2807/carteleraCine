7e5f34b4b3013100ce6c0bcb030e9d3938ea3087
Alexander <alexisparedes_2019@hotmail.com>
2024-06-06 13:13:39 +0200

    Refactorizando codigo

--- src/app/componentes/apiPeliculas/pelicula-api.component.ts
+++ src/app/componentes/apiPeliculas/pelicula-api.component.ts
@@ -20,313 +20,120 @@ export class PeliculaApiComponent implements OnInit {
     private salaServicio: SalaService,
   ) {}
 
-  fechaActual = new Date('2024-06-13T09:00:00Z');
-  fechaFinal = new Date('2024-06-28T23:59:59Z');
+  fechaActual = new Date('2024-06-11T09:00:00Z');
+  fechaFinal = new Date('2024-06-12T23:59:59Z');
   sala = 1;
 
   ngOnInit(): void {
     this.peliculaServicio.obtenerPeliculas().subscribe({
       next: (peliculas) => {
-        let cont = 0;
+        console.log(peliculas.length);
         while (this.fechaActual <= this.fechaFinal) {
           const fechaConstActual = new Date(this.fechaActual);
-          this.sala = 1;
-          cont = 0;
-          peliculas.forEach((pelicula) => {
-            const horaActual = this.fechaActual.getUTCHours();
-            const minutosActual = this.fechaActual.getUTCMinutes();
-            const segundosActual = this.fechaActual.getUTCSeconds();
-            if (
-              horaActual > 8 ||
-              (horaActual === 8 && minutosActual > 59) ||
-              (horaActual === 8 && minutosActual === 59 && segundosActual > 59)
-            ) {
-              const horaInicio = this.fechaActual.toUTCString().split(' ')[4];
-              const fecha = this.fechaActual.toISOString().split('T')[0];
 
-              this.fechaActual.setMinutes(
-                this.fechaActual.getMinutes() + pelicula.duracion,
-              );
-              const horaFinal = this.fechaActual.toUTCString().split(' ')[4];
+          this.procesarPeliculas(peliculas);
 
-              /* const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-              console.log(
-                'pelicula: ' +
-                  pelicula.titulo +
-                  ' horaFinal: ' +
-                  horaFinal +
-                  ' horainciio:; ' +
-                  horaInicio +
-                  ' sala: ' +
-                  this.sala +
-                  ' duracion: ' +
-                  pelicula.duracion +
-                  ' fecha: ' +
-                  fecha +
-                  ' precio: ' +
-                  precio,
-              ); */
+          this.procesarPeliculas(peliculas.reverse());
 
-              /* this.salaServicio.obtenerSala(this.sala).subscribe({
-                next: (sala) => {
-                  const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-                  this.sesionPeliculaServicio
-                    .guardarSesion(
-                      pelicula,
-                      sala,
-                      fecha,
-                      horaInicio,
-                      horaFinal,
-                      precio,
-                    )
-                    .subscribe({
-                      next: (sesionGuardada) => {
-                        console.log(sesionGuardada);
-                      },
-                      error: (er) => {
-                        console.log(er);
-                      },
-                    });
-                },
-              }); */
-
-              this.fechaActual.setMinutes(this.fechaActual.getMinutes() + 10);
-              cont++;
-            } else {
-              /* console.log(
-                '----------------------------------------------------------------------------------',
-              ); */
-              this.sala++;
-              const fechaActualUTC = new Date(
-                this.fechaActual.getTime() +
-                  this.fechaActual.getTimezoneOffset() * 60000,
-              );
-              fechaActualUTC.setHours(9, 0, 0, 0);
-
-              fechaActualUTC.setDate(fechaActualUTC.getDate() - 1);
-
-              this.fechaActual = new Date(
-                fechaActualUTC.getTime() -
-                  fechaActualUTC.getTimezoneOffset() * 60000,
-              );
-              const horaInicio = this.fechaActual.toUTCString().split(' ')[4];
-
-              const fecha = this.fechaActual.toISOString().split('T')[0];
-              this.fechaActual.setMinutes(
-                this.fechaActual.getMinutes() + pelicula.duracion,
-              );
-              const horaFinal = this.fechaActual.toUTCString().split(' ')[4];
-
-              /* const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-              console.log(
-                'pelicula: ' +
-                  pelicula.titulo +
-                  ' horaFinal: ' +
-                  horaFinal +
-                  ' horainciio:; ' +
-                  horaInicio +
-                  ' sala: ' +
-                  this.sala +
-                  ' duracion: ' +
-                  pelicula.duracion +
-                  ' fecha: ' +
-                  fecha +
-                  ' precio: ' +
-                  precio,
-              ); */
-
-              /* this.salaServicio.obtenerSala(this.sala).subscribe({
-                next: (sala) => {
-                  const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-                  this.sesionPeliculaServicio
-                    .guardarSesion(
-                      pelicula,
-                      sala,
-                      fecha,
-                      horaInicio,
-                      horaFinal,
-                      precio,
-                    )
-                    .subscribe({
-                      next: (sesionGuardada) => {
-                        console.log(sesionGuardada);
-                      },
-                      error: (er) => {
-                        console.log(er);
-                      },
-                    });
-                },
-              }); */
-
-              this.fechaActual.setMinutes(this.fechaActual.getMinutes() + 10);
-              cont++;
-            }
-          });
-
-          cont = 0;
-          const fechaActualUTC = new Date(
-            this.fechaActual.getTime() +
-              this.fechaActual.getTimezoneOffset() * 60000,
-          );
-          fechaActualUTC.setHours(9, 0, 0, 0);
-
-          fechaActualUTC.setDate(fechaActualUTC.getDate());
-
-          this.fechaActual = new Date(
-            fechaActualUTC.getTime() -
-              fechaActualUTC.getTimezoneOffset() * 60000,
-          );
-          this.sala++;
-          /* console.log(
-            'REVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSEREVERSE',
-          ); */
-
-          peliculas.reverse().forEach((pelicula) => {
-            const horaActual = this.fechaActual.getUTCHours();
-            const minutosActual = this.fechaActual.getUTCMinutes();
-            const segundosActual = this.fechaActual.getUTCSeconds();
-            if (
-              horaActual > 8 ||
-              (horaActual === 8 && minutosActual > 59) ||
-              (horaActual === 8 && minutosActual === 59 && segundosActual > 59)
-            ) {
-              const horaInicio = this.fechaActual.toUTCString().split(' ')[4];
-              const fecha = this.fechaActual.toISOString().split('T')[0];
-
-              this.fechaActual.setMinutes(
-                this.fechaActual.getMinutes() + pelicula.duracion,
-              );
-              const horaFinal = this.fechaActual.toUTCString().split(' ')[4];
-
-              /* const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-              console.log(
-                'pelicula: ' +
-                  pelicula.titulo +
-                  ' horaFinal: ' +
-                  horaFinal +
-                  ' horainciio:; ' +
-                  horaInicio +
-                  ' sala: ' +
-                  this.sala +
-                  ' duracion: ' +
-                  pelicula.duracion +
-                  ' fecha: ' +
-                  fecha +
-                  ' precio: ' +
-                  precio,
-              ); */
-
-              /* this.salaServicio.obtenerSala(this.sala).subscribe({
-                next: (sala) => {
-                  const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-                  this.sesionPeliculaServicio
-                    .guardarSesion(
-                      pelicula,
-                      sala,
-                      fecha,
-                      horaInicio,
-                      horaFinal,
-                      precio,
-                    )
-                    .subscribe({
-                      next: (sesionGuardada) => {
-                        console.log(sesionGuardada);
-                      },
-                      error: (er) => {
-                        console.log(er);
-                      },
-                    });
-                },
-              }); */
-
-              this.fechaActual.setMinutes(this.fechaActual.getMinutes() + 10);
-              cont++;
-            } else {
-              /* console.log(
-                '----------------------------------------------------------------------------------',
-              ); */
-              this.sala++;
-              const fechaActualUTC = new Date(
-                this.fechaActual.getTime() +
-                  this.fechaActual.getTimezoneOffset() * 60000,
-              );
-              fechaActualUTC.setHours(9, 0, 0, 0);
-
-              fechaActualUTC.setDate(fechaActualUTC.getDate() - 1);
-
-              this.fechaActual = new Date(
-                fechaActualUTC.getTime() -
-                  fechaActualUTC.getTimezoneOffset() * 60000,
-              );
-              const horaInicio = this.fechaActual.toUTCString().split(' ')[4];
-
-              const fecha = this.fechaActual.toISOString().split('T')[0];
-              this.fechaActual.setMinutes(
-                this.fechaActual.getMinutes() + pelicula.duracion + 10,
-              );
-              const horaFinal = this.fechaActual.toUTCString().split(' ')[4];
-
-              /* const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-              console.log(
-                'pelicula: ' +
-                  pelicula.titulo +
-                  ' horaFinal: ' +
-                  horaFinal +
-                  ' horainciio:; ' +
-                  horaInicio +
-                  ' sala: ' +
-                  this.sala +
-                  ' duracion: ' +
-                  pelicula.duracion +
-                  ' fecha: ' +
-                  fecha +
-                  ' precio: ' +
-                  precio,
-              ); */
-
-              /* this.salaServicio.obtenerSala(this.sala).subscribe({
-                next: (sala) => {
-                  const precio = Math.floor(Math.random() * (12 - 6)) + 6.99;
-                  this.sesionPeliculaServicio
-                    .guardarSesion(
-                      pelicula,
-                      sala,
-                      fecha,
-                      horaInicio,
-                      horaFinal,
-                      precio,
-                    )
-                    .subscribe({
-                      next: (sesionGuardada) => {
-                        console.log(sesionGuardada);
-                      },
-                      error: (er) => {
-                        console.log(er);
-                      },
-                    });
-                },
-              }); */
-
-              this.fechaActual.setMinutes(this.fechaActual.getMinutes() + 10);
-              cont++;
-            }
-          });
-
-          const fechaSiguiente = new Date(
-            fechaConstActual.getTime() +
-              fechaConstActual.getTimezoneOffset() * 60000,
-          );
-          fechaSiguiente.setDate(fechaSiguiente.getDate() + 1);
-
-          this.fechaActual = new Date(
-            fechaSiguiente.getTime() -
-              fechaSiguiente.getTimezoneOffset() * 60000,
-          );
+          this.fechaActual = this.incrementarDia(fechaConstActual);
 
           peliculas.sort(() => Math.random() - 0.5);
+          this.sala = 1;
         }
       },
     });
+  }
+
+  private procesarPeliculas(peliculas: any[]): void {
+    peliculas.forEach((pelicula) => {
+      if (this.horaEsValida()) {
+        this.procesarPelicula(pelicula);
+      } else {
+        this.sala++;
+        this.resetearFechaParaNuevaSala();
+        this.procesarPelicula(pelicula);
+      }
+    });
+  }
+
+  private horaEsValida(): boolean {
+    const horaActual = this.fechaActual.getUTCHours();
+    const minutosActual = this.fechaActual.getUTCMinutes();
+    const segundosActual = this.fechaActual.getUTCSeconds();
+    return (
+      horaActual > 8 ||
+      (horaActual === 8 && minutosActual >= 59 && segundosActual >= 59)
+    );
+  }
+
+  private procesarPelicula(pelicula: any): void {
+    const horaInicio = this.obtenerHoraActual();
+    const fecha = this.obtenerFechaActual();
+
+    this.fechaActual.setMinutes(
+      this.fechaActual.getMinutes() + pelicula.duracion,
+    );
+    const horaFinal = this.obtenerHoraActual();
+    const precio = this.generarPrecioAleatorio();
+
+/*     console.log(
+      `pelicula: ${pelicula.titulo} horaFinal: ${horaFinal} horainciio: ${horaInicio} sala: ${this.sala} duracion: ${pelicula.duracion} fecha: ${fecha} precio: ${precio}`,
+    ); */this.salaServicio.obtenerSala(this.sala).subscribe({
+      next: (sala) => {
+        this.sesionPeliculaServicio
+          .guardarSesion(
+            pelicula,
+            sala,
+            fecha,
+            horaInicio,
+            horaFinal,
+            precio,
+          )
+          .subscribe({
+            next: (sesionGuardada) => {
+              console.log(sesionGuardada);
+            },
+            error: (er) => {
+              console.log(er);
+            },
+          });
+      },
+    });
+
+    this.fechaActual.setMinutes(this.fechaActual.getMinutes() + 10);
+  }
+
+  private resetearFechaParaNuevaSala(): void {
+    const fechaActualUTC = new Date(
+      this.fechaActual.getTime() + this.fechaActual.getTimezoneOffset() * 60000,
+    );
+    fechaActualUTC.setHours(9, 0, 0, 0);
+    fechaActualUTC.setDate(fechaActualUTC.getDate() - 1);
+    this.fechaActual = new Date(
+      fechaActualUTC.getTime() - fechaActualUTC.getTimezoneOffset() * 60000,
+    );
+  }
+
+  private incrementarDia(fechaConstActual: Date): Date {
+    const fechaSiguiente = new Date(
+      fechaConstActual.getTime() + fechaConstActual.getTimezoneOffset() * 60000,
+    );
+    fechaSiguiente.setDate(fechaSiguiente.getDate() + 1);
+    return new Date(
+      fechaSiguiente.getTime() - fechaSiguiente.getTimezoneOffset() * 60000,
+    );
+  }
+
+  private obtenerHoraActual(): string {
+    return this.fechaActual.toUTCString().split(' ')[4];
+  }
+
+  private obtenerFechaActual(): string {
+    return this.fechaActual.toISOString().split('T')[0];
+  }
+
+  private generarPrecioAleatorio(): number {
+    return Math.floor(Math.random() * (12 - 6)) + 6.99;
   }
 
   /* ngOnInit(): void {
